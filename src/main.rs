e2aab5ec934d2bc0d3cd5ebdb25431cdc30210dc
Colton Robertson <colt.robertson86@gmail.com>
2024-03-12 22:41:08 -0500

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,10 @@
-use league_client::client;
-use league_client::connector::subscribe;
-use league_client::core::EventType;
-
-use rss::*;
-
 #[tokio::main]
 async fn main() {
-    let builder: client::ClientBuilder;
+    env_logger::init();
 
     loop {
-        match client::Client::builder() {
-            Err(_) => {
-                tracing::info!("waiting for client to be active");
-                let ten_secs = std::time::Duration::from_secs(10);
-                std::thread::sleep(ten_secs);
-            }
-            Ok(b) => {
-                builder = b;
-                break;
-            }
-        }
-    }
-
-    println!("we're connected");
-
-    let c = builder.insecure(true).build().unwrap();
-
-    let connected = c.connect_to_socket().await.unwrap();
-    let speaker = subscribe(connected).await;
-    let http_client = c.http_client();
-
-    let state = rss::Locker::new(http_client, c.addr.clone())
-        .await
-        .expect("failed to create the http client");
-    let state: State = state.into();
-
-    let msg = (5, "OnJsonApiEvent");
-    let msg = serde_json::to_string(&msg).unwrap();
-    speaker.send(msg).await.expect("should have sent a message");
-
-    let (session_tx, session_rx) = flume::unbounded();
-    let _handle = tokio::task::spawn(rss::current_spells_and_ward_skin(state.clone(), session_rx));
-
-    let mut counter = 0;
-    while let Ok(incoming) = speaker.reader.recv_async().await {
-        let message = incoming.into_message();
-
-        if &message.uri == "/lol-champ-select/v1/session" {
-            if let Err(e) = session_tx.send(message) {
-                tracing::warn!("failed to send {e}");
-            }
-
-            continue;
-        }
-
-        // Skip unwanted stuff.
-        if &message.uri != "/lol-champ-select/v1/skin-carousel-skins" {
-            continue;
-        }
-
-        let skins = serde_json::from_value::<Vec<rss::CarouselSkin>>(message.data);
-
-        if skins.is_err() {
-            continue;
-        }
-
-        let skins = skins.unwrap();
-
-        if skins.is_empty() {
-            continue;
-        }
-
-        let _ = tokio::time::sleep(tokio::time::Duration::from_secs(3));
-
-        let skin = select_skin(state.clone(), skins).await.unwrap();
-        if let Err(e) = state.read().await.pick_this_skin(skin).await {
-            println!("failed to pick skin {e}");
-            continue;
+        if let Err(e) = rss::run().await {
+            log::error!("run stopped: {e:?}");
         }
     }
 }
--- src/rss.rs
+++ src/rss.rs
@@ -3,6 +3,10 @@ use rand::prelude::SliceRandom;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
+use league_client::client;
+use league_client::connector::subscribe;
+// use league_client::core::EventType;
+
 pub type State = Arc<RwLock<Locker>>;
 
 #[derive(Default)]
@@ -19,22 +23,32 @@ impl From<Locker> for State {
     }
 }
 
+fn tries(mut counter: u64) -> tokio::time::Duration {
+    log::trace!("attempt {counter}");
+    if counter > 3 {
+        counter = 3;
+    }
+
+    tokio::time::Duration::from_secs(counter * 10)
+}
+
 impl Locker {
     pub async fn new(client: reqwest::Client, addr: String) -> Result<Self> {
         let req = format!("https://{}/{}", &addr, "lol-summoner/v1/current-summoner");
 
         let session: super::core::Session;
+
+        let mut counter: u64 = 1;
         loop {
             let resp = client.get(&req).send().await?.text().await?;
             if let Ok(s) = serde_json::from_str::<super::core::Session>(&resp) {
                 session = s;
                 break;
             }
-            tracing::info!("waiting for the user to log in...");
-            let _ = tokio::time::sleep(tokio::time::Duration::from_secs(5));
-        }
 
-        println!("we're logged in");
+            tokio::time::sleep(tries(counter)).await;
+            counter += 1;
+        }
 
         Ok(Self {
             client,
@@ -42,10 +56,6 @@ impl Locker {
             addr,
             ..Default::default()
         })
-    }
-
-    fn update_session(&mut self, other: super::core::Session) {
-        self.session = other;
     }
 
     fn update_select(&mut self, other: super::core::Select) {
@@ -57,7 +67,7 @@ impl Locker {
     }
 
     pub fn select(&self) -> super::core::Select {
-        let mut select = self.select.clone();
+        let select = self.select.clone();
 
         select
     }
@@ -79,7 +89,8 @@ pub async fn current_spells_and_ward_skin(
     state: State,
     rx: flume::Receiver<league_client::core::Message>,
 ) -> Result<()> {
-    while let Ok(msg) = rx.recv_async().await {
+    loop {
+        let msg = rx.recv_async().await?;
         let summoner_id = state.read().await.summoner_id();
 
         let selection: super::core::Selection = serde_json::from_value(msg.data)?;
@@ -88,12 +99,10 @@ pub async fn current_spells_and_ward_skin(
                 continue;
             }
 
-            let mut select = super::core::Select::from(player.clone());
+            let select = super::core::Select::from(player.clone());
             state.write().await.update_select(select);
         }
     }
-
-    Ok(())
 }
 
 pub async fn select_skin(
@@ -118,4 +127,112 @@ pub async fn select_skin(
     select.selected_skin_id = selected_skin_id;
 
     Ok(select)
+}
+
+struct Consumer {
+    state: State,
+    speaker: league_client::Speaker,
+    session_tx: flume::Sender<league_client::core::Message>,
+}
+
+impl Consumer {
+    pub async fn spawn(&mut self) -> Result<()> {
+        let msg = (5, "OnJsonApiEvent");
+        let msg = serde_json::to_string(&msg)?;
+        self.speaker.send(msg).await?;
+
+        while let Ok(incoming) = self.speaker.reader.recv_async().await {
+            let message = incoming.into_message();
+
+            if &message.uri == "/lol-champ-select/v1/session" {
+                if let Err(e) = self.session_tx.send(message) {
+                    log::warn!("failed to send {e}");
+                }
+
+                continue;
+            }
+
+            // Skip unwanted stuff.
+            if &message.uri != "/lol-champ-select/v1/skin-carousel-skins" {
+                continue;
+            }
+
+            let skins = serde_json::from_value::<Vec<crate::CarouselSkin>>(message.data);
+
+            if skins.is_err() {
+                continue;
+            }
+
+            let skins = skins.unwrap();
+
+            if skins.is_empty() {
+                continue;
+            }
+
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+
+            let skin = match select_skin(self.state.clone(), skins).await {
+                Ok(skin) => skin,
+                Err(e) => {
+                    log::error!("failed to select the skin: {:?}", e);
+                    continue;
+                }
+            };
+
+            if let Err(e) = self.state.read().await.pick_this_skin(skin).await {
+                log::error!("failed to pick skin: {e}");
+                continue;
+            }
+        }
+
+        anyhow::bail!("speaker was dropped");
+    }
+}
+
+pub async fn run() -> Result<()> {
+    let builder: client::ClientBuilder;
+
+    let mut counter: u64 = 1;
+    loop {
+        match client::Client::builder() {
+            Err(_) => {
+                log::info!("waiting for client to be active");
+                tokio::time::sleep(tries(counter)).await;
+            }
+            Ok(b) => {
+                builder = b;
+                break;
+            }
+        }
+
+        counter += 1;
+    }
+
+    let c = builder.insecure(true).build().unwrap();
+
+    let connected = c.connect_to_socket().await.unwrap();
+    let speaker = subscribe(connected).await;
+    let http_client = c.http_client();
+
+    let state = crate::Locker::new(http_client, c.addr.clone())
+        .await
+        .expect("failed to create the http client");
+    let state: State = state.into();
+
+    let (session_tx, session_rx) = flume::unbounded();
+    let _handle_1 = tokio::task::spawn(crate::current_spells_and_ward_skin(
+        state.clone(),
+        session_rx,
+    ));
+
+    let mut consumer = Consumer {
+        state,
+        speaker,
+        session_tx,
+    };
+
+    log::info!("rolling the dice");
+    consumer.spawn().await?;
+
+    anyhow::bail!("run stopped early");
 }
